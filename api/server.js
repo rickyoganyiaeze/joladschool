190ddf948274c03c5ff6b564d8c69154c0223539
rickyoganyiaeze <rickyoganyiaeze@gmail.com>
2026-04-03 12:55:37 +0100

    Test API

--- api/server.js
+++ api/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
+// This is a simple test to see if Vercel works
 app.get('/hello', (req, res) => {
-  res.json({ message: 'It Works!' });
+  res.json({ message: 'API is Working!' });
 });
 
 module.exports = app;
