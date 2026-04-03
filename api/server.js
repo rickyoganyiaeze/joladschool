f79cb52141babd7df9fc400b51f93a872a9ab7ec
rickyoganyiaeze <rickyoganyiaeze@gmail.com>
2026-04-03 13:01:48 +0100

    Force Vercel functions

--- api/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-// This is a simple test to see if Vercel works
-app.get('/hello', (req, res) => {
-  res.json({ message: 'API is Working!' });
-});
-
-module.exports = app;
